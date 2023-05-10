d5e14dba7c9e534a5218a8c804e8b30e0fa88b2c
Onesmus Wambugu <onesmus1024@gmail.com>
2023-05-10 15:53:34 +0300

    update: add background image

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./home.css";
 
-
 function Home() {
   return (
     <div className="home">
       <h1 className="home__title">Algo Trading</h1>
-        <p>Algo Trading is a web application that allows users to create and backtest trading algorithms.</p>
-        
+      <p>
+        Algo Trading is a web application that allows users to create and
+        backtest trading algorithms.
+      </p>
     </div>
   );
 }
-
-
 
 export default Home;
